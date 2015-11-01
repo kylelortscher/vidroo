45867ac09a1dd5a0251b4e4704c4e91a5e86b5a5
Kyle Lortscher <kylelortscher@gmail.com>
2015-10-31 19:47:03 -0700

    Added video regex, and validations for video form

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,3 +1,5 @@
 class Video < ActiveRecord::Base
 	validates :title, presence: true, length: {in: 3..50 }
+	VALID_URL_REGEX = /(youtu\.be\/|yoube\.com\/(watc\?(.*&)?v=|(embed|v)\/))([^\?&"'>]+)/
+	validates :url, presence: true, uniqueness: { case_sensitive: false }, length: {in: 9..50}, format: { with: VALID_URL_REGEX }
 end
